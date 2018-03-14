f70d8526248d495eec2c03515d3413f21397ca36
Julio Bastidas <jbastidas@laboratoriosmiranda.gob.ve>
2018-03-14 15:16:46 -0400

    Prueba del boton de login con google

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import gapi from 'gapi-client';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,6 +11,17 @@ export class AppComponent {
   title = 'app';
 
   LoginGoogle(){
+    gapi.load('client:auth2', this.initClient);
     console.log("google");
+  }
+
+  initClient() {
+    gapi.client.init({
+      discoveryDocs: ["https://www.googleapis.com/discovery/v1/apis/drive/v3/rest"],
+      clientId: '572404916269-npse5kiflg8f74jrr7eub9c87ptg49kj.apps.googleusercontent.com',
+      scope: 'https://www.googleapis.com/auth/drive.metadata.readonly'
+    }).then(function (){
+      console.log("Funciona");
+    });  
   }
 }
